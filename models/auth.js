5dde2fa4e6a860d54881cd03f5d0cfb10ebf93f2
ihasnainbhutta <ihassnainbhutta@gmail.com>
2023-02-09 19:23:04 +0500

    Add bcryptjs instead of bcrypt

--- models/auth.js
+++ models/auth.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const client = require("../config/dbConnection");
 const jwt = require("jsonwebtoken");
 const env = require("dotenv");
